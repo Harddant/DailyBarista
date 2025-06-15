6164322c51a28f63a7515d7a7995c0a422498bda
Harddant <rt.dekker@outlook.com>
2025-06-15 21:40:26 +0200

    Added icons for the tab navigation and removed the title for a cleaner look

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
@@ -29,8 +30,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: '',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -50,15 +51,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="recommended"
         options={{
-          title: 'Recommended',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: '',
+          tabBarIcon: ({ color }) => <TabBarIcon name="coffee" color={color} />,
         }}
       />
         <Tabs.Screen
             name="saved"
             options={{
-                title: 'Saved',
-                tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+                title: '',
+                tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={25}/>,
             }}
         />
     </Tabs>
